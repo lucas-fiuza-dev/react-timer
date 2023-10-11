507013e47f049502bfe0176fd24d4bf7e8306d72
Lucas Fiúza <lucas.fiuza.developer@gmail.com>
2023-10-11 20:33:28 -0300

    commit-15kg

--- src/Routerr.tsx
+++ src/Routerr.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/home'
-import { History } from './pages/history'
-import { DefaultLayout } from './layouts/DefaultLayout'
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/home"
+import { History } from "./pages/history"
+import { DefaultLayout } from "./layouts/defaultlayout"
 export function Router() {
   return (
     <Routes>
